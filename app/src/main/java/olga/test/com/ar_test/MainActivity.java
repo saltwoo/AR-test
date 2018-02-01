97ee257802c6f44bcc4d00bb3e67c9bf83dec755
Altug Guner <altugguner90@gmail.com>
2018-02-01 10:54:40 +0100

    Inital project commit

---
+++ app/src/main/java/olga/test/com/ar_test/MainActivity.java
@@ -0,0 +1,13 @@
+package olga.test.com.ar_test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
